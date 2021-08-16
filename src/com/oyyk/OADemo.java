fd914881e6c3e8aea635e063675d0a3fcdb96150
oyyk <745691616@qq.com>
2021-08-16 15:51:56 +0800

    初始代码

--- src/com/oyyk/OADemo.java
+++ src/com/oyyk/OADemo.java
@@ -1,17 +1,18 @@
 package com.oyyk;
 
 public class OADemo {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         Double salary = 2000.00;
         Double money;
         while(true){
             money = calSalary(salary);
-            System.out.println(String.format("实际到手工资：{}", money));
+            System.out.println("实际到手工资：" + money);
             Thread.sleep(1000);
         }
     }
 
     private static Double calSalary(Double salary){
-        return 1.00;
+        SalaryCaler caler = new SalaryCaler();
+        return caler.cal(salary);
     }
 }
---
+++ src/com/oyyk/SalaryCaler.java
@@ -0,0 +1,7 @@
+package com.oyyk;
+
+public class SalaryCaler {
+    public Double cal(Double salary){
+        return salary*1.2;
+    }
+}
