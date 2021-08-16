32aff08cf3971739a175e33cff61116ab7b294c5
oyyk <745691616@qq.com>
2021-08-16 16:05:29 +0800

    代码拆分到jar包

--- src/com/oyyk/OADemo.java
+++ src/com/oyyk/OADemo.java
@@ -1,18 +1,26 @@
 package com.oyyk;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 public class OADemo {
     public static void main(String[] args) throws Exception {
         Double salary = 2000.00;
         Double money;
+        URL jarPath = new URL("file:D:\\:lib\\SalaryCaler.jar");
+        URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{jarPath});
         while(true){
-            money = calSalary(salary);
+            money = calSalary(salary, urlClassLoader);
             System.out.println("实际到手工资：" + money);
             Thread.sleep(1000);
         }
     }
 
-    private static Double calSalary(Double salary){
-        SalaryCaler caler = new SalaryCaler();
-        return caler.cal(salary);
+    private static Double calSalary(Double salary, ClassLoader classLoader) throws Exception {
+//        SalaryCaler caler = new SalaryCaler();
+//        return caler.cal(salary);
+        Class<?> clazz = classLoader.loadClass("com.oyyk.SalaryCaler");
+        Object object = clazz.newInstance();
+        return (Double) clazz.getMethod("cal", Double.class).invoke(object, salary);
     }
 }
